d05b7577edcf524582d823b43a688f873f52756f
xusanyusupov <yusupov.xusan.org@gmail.com>
2024-09-08 01:11:51 +0500

    codewars

---
+++ main.js
@@ -0,0 +1,104 @@
+////// MASALA 01
+// {
+//     const foo = word => typeof word === 'string' ? word : word.toString();
+//     console.log(foo(123456));
+// }
+
+
+//////MASALA 02
+// {
+//     const foo =  num => num % 2 === 0 ? 'juft' : 'toq'
+//     console.log(foo(7));
+// }
+
+//////MASALA 03
+// {  
+// const foo = soz => soz.split('').reverse().join('')
+// console.log(foo("Assalomu aleykum")); 
+// }
+
+///// MASALA 04
+
+// {
+//     const  opposite = num => -num
+// console.log(opposite(5));
+// }
+
+///// MASALA 05
+// {
+//     function musbatSonlarniQosh(arr) {
+//         let sum = 0;
+//         for (let i = 0; i < arr.length; i++) {
+//           if (arr[i] > 0) {
+//             sum += arr[i];
+//           }
+//         }
+//         return sum;
+//       }
+      
+//       const raqamlar = [-10, 10, 20, ,30];
+//       console.log(musbatSonlarniQosh(raqamlar)); // Natija: 65
+        
+// }
+
+//////MASALA 06
+// {
+//     function helloI(str) {
+//         let count = 0;
+//         for (let i = 0; i < str.length; i++) {
+//           if (str[i] === 'i') {
+//             count++;
+//           }
+//         }
+//         for (let s = 0; s < count; s++) {
+//           console.log("hello");
+//         }
+//       }
+//       const matn = "iiii";
+//       helloI(matn)      
+// }
+
+////// MASALA 07
+// {
+//     function raqam(son) {
+//         return son * son 
+//     }
+//     console.log(raqam(20));
+// }
+
+
+////// MASALA 08
+// {
+//     function songachaQosh(son) {
+//         let sum = 0;
+//         for (let i = 1; i <= son; i++) {
+//           sum += i;
+//         }
+//         return sum;
+//       }
+      
+//       console.log(songachaQosh(5));  
+//       console.log(songachaQosh(10)); 
+      
+// }
+
+////// MASALA 09
+// {
+//     const numbers = [5, 10, 3, 8];
+//     console.log(Math.min(...numbers)); // Natija: 3
+
+// }
+
+
+////// MASALA 10
+// {
+//     let sum = 0
+//     for(let i = 0; i<= 10; i++){
+//         sum+= i
+//     }
+//     console.log(sum);
+// }
+
+
+
+
